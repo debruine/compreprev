a83110c5ed1415f728cdbdc4c4430f15269f4599
debruine <debruine@gmail.com>
2024-03-02 10:41:18 +0100

    contributors 👩‍🔧

--- inst/app/tabs/tips_tab.R
+++ inst/app/tabs/tips_tab.R
@@ -20,5 +20,6 @@ devtools::install_github(\"debruine/compreprev\")
        <p>Access the <a href='https://rstudio-connect.psy.gla.ac.uk/compreprev/'>shiny app online</a> or <a href='https://github.com/debruine/compreprev/issues'>file issues on the github repo</a>.</p>"),
   
   h3("Contributors"),
-  p("")
+  p("The initial development of this app happened at the Garage Project meeting in Melbourne, Australia in December 2023, in a small group of Lisa DeBuine, Tom Hardwicke, Frederick Aust, and Richard Lucas."),
+  p("The app was evaulated by a hackathon group at the Perspectives on Scientific Error meeting in Eindhoven, NL in March 2024.")
 )
